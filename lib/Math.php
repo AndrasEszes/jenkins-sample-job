eca9d2fa46e4b40574a10e07339707c6917f1a39
Andras Eszes <ebandee@gmail.com>
2012-12-11 13:05:17 +0100

    Fundamental mathematical functions created with tests

--- lib/Math.php
+++ lib/Math.php
@@ -1,17 +1,43 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Math
- *
- * @author andras.eszes
+ * @author <andras.eszes@quantislabs.com>
  */
 class Math {
         //put your code here
+        public function summation() {
+                return array_sum(func_get_args());
+        }
+        
+        public function subtraction() {
+                $args = func_get_args();
+                $ret = (isset($args[0])) ? $args[0] : 0;
+                for($i=1;$i<count($args);$i++) {
+                        $ret -= $args[$i];
+                }
+                return $ret;
+        }
+        
+        public function multiplication() {
+                $ret = 1;
+                foreach( func_get_args() as $val ) {
+                        $ret *= $val;
+                }
+                return $ret;
+        }
+        
+        public function division() {
+                $args = func_get_args();
+                $ret = (isset($args[0])) ? $args[0] : 0;
+                if( $ret == 0 )
+                        return 0;
+                else {
+                        for($i=1;$i<count($args);$i++) {
+                                if( $args[$i] == 0 ) return 'Division by zero!';
+                                $ret /= $args[$i];
+                        }
+                }
+                return $ret;
+        }
 }
 
 ?>
--- tests/MathTest.php
+++ tests/MathTest.php
@@ -1,20 +1,54 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of MathTest
- *
- * @author andras.eszes
- */
 class MathTest extends PHPUnit_Framework_TestCase {
         private $_math;
-        public function testCreateObject() {
+        
+        protected function setUp() {
                 $this->_math = new Math();
+        }
+        
+        public function testCreateObject() {
                 $this->assertTrue($this->_math instanceof Math);
+        }
+        
+        public function testSummation() {
+                $this->assertEquals(0,$this->_math->summation(0,0));
+                $this->assertEquals(5,$this->_math->summation(2,3));
+                $this->assertEquals(0,$this->_math->summation(-10,+10));
+                $this->assertEquals(5.12,$this->_math->summation(2.07,3.05));
+                $this->assertEquals(8.58,$this->_math->summation(2.07,3.05,-2.12,5.58));
+                $this->assertEquals(0, $this->_math->summation('foo','bar'));
+                $this->assertEquals(14, $this->_math->summation('foo','bar',14));
+        }
+        
+        public function testSubtraction() {
+                $this->assertEquals(0,$this->_math->subtraction(0,0));
+                $this->assertEquals(-1,$this->_math->subtraction(2,3));
+                $this->assertEquals(-20,$this->_math->subtraction(-10,+10));
+                $this->assertEquals(-0.98,$this->_math->subtraction(2.07,3.05));
+                $this->assertEquals(-4.44,$this->_math->subtraction(2.07,3.05,-2.12,5.58));
+                $this->assertEquals(0, $this->_math->subtraction('foo','bar'));
+                $this->assertEquals(-14, $this->_math->subtraction('foo','bar',14));
+        }
+        
+        public function testMultiplication() {
+                $this->assertEquals(0,$this->_math->multiplication(0,0));
+                $this->assertEquals(6,$this->_math->multiplication(2,3));
+                $this->assertEquals(-100,$this->_math->multiplication(-10,+10));
+                $this->assertEquals(6.3135,$this->_math->multiplication(2.07,3.05));
+                $this->assertEquals(-74.6861796,$this->_math->multiplication(2.07,3.05,-2.12,5.58));
+                $this->assertEquals(0, $this->_math->multiplication('foo','bar'));
+                $this->assertEquals(0, $this->_math->multiplication('foo','bar',14));
+        }
+        
+        public function testDivision() {
+                $this->assertEquals(0,$this->_math->division(0,0));
+                $this->assertEquals('Division by zero!',$this->_math->division(11,3,0,2));
+                $this->assertEquals(2/3,$this->_math->division(2,3));
+                $this->assertEquals(-1,$this->_math->division(-10,+10));
+                $this->assertEquals(2.07/3.05,$this->_math->division(2.07,3.05));
+                $this->assertEquals(2.07/3.05/-2.12/5.58,$this->_math->division(2.07,3.05,-2.12,5.58));
+                $this->assertEquals(0, $this->_math->division('foo','bar'));
+                $this->assertEquals(0, $this->_math->division('foo','bar',14));
         }
 }
 
