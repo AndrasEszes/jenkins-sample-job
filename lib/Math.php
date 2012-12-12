b93930003ec8d002cd5f3a1fc1a832728f71778c
Andras Eszes <ebandee@gmail.com>
2012-12-12 11:53:22 +0100

    Doc comments created in Math class

--- lib/Math.php
+++ lib/Math.php
@@ -1,13 +1,37 @@
 <?php
 /**
- * @author <andras.eszes@quantislabs.com>
+ * A Math osztály egy matematikai műveleteket megvalósító osztály
+ *
+ * Használat:
+ * $math = new Math();
+ * echo $math->summation(1,2,3,4,5); // 15
+ * echo $math->division(12,2,3) // 2
+ *
+ * @package  Mathematical
+ * @author   Eszes András <andras.eszes@quantislabs.com>
+ * @version  $Revision: 1.0 $
+ * @access   public
  */
 class Math {
-        //put your code here
+        
+        /**
+         * A paraméterben átadott számok összege. A paraméterben kapott elemeket összadja, ha lehetséges.
+         * 
+         * @access public
+         * @param int|float
+         * @return int|float
+         */
         public function summation() {
                 return array_sum(func_get_args());
         }
         
+        /**
+         * A paraméterben kapott számokat kivonja az első paraméterként kapott számból.
+         * 
+         * @access public
+         * @param int|float
+         * @return int|float
+         */
         public function subtraction() {
                 $args = func_get_args();
                 $ret = (isset($args[0])) ? $args[0] : 0;
@@ -17,6 +41,13 @@ class Math {
                 return $ret;
         }
         
+        /**
+         * A paraméterként kapott elemek szorzatát adja vissza
+         * 
+         * @access public
+         * @param int|float
+         * @return int|float
+         */
         public function multiplication() {
                 $ret = 1;
                 foreach( func_get_args() as $val ) {
@@ -25,6 +56,14 @@ class Math {
                 return $ret;
         }
         
+        /**
+         * Ez első paraméterben adott számot elosztja az utána következőkkel.
+         * Nullával való osztás esetén "Division by zero!" üzenetet ad vissza.
+         * 
+         * @access public
+         * @param int|float
+         * @return int|float|string
+         */
         public function division() {
                 $args = func_get_args();
                 $ret = (isset($args[0])) ? $args[0] : 0;
@@ -39,6 +78,13 @@ class Math {
                 return $ret;
         }
         
+        /**
+         * A paraméterben kapott számok átlagát adja vissza.
+         * 
+         * @access public
+         * @param int|float
+         * @return int|float
+         */
         public function avg() {
                 $sum = array_sum(func_get_args());
                 $db = func_num_args();
@@ -47,6 +93,14 @@ class Math {
                 return $sum/$db;
         }
         
+        /**
+         * A hatványozás műveletet valósítja meg.
+         * 
+         * @access public
+         * @param int|float $base
+         * @param int|float $power
+         * @return int|float
+         */
         public function exponentiation($base,$power) {
                 return pow($base,$power);
         }
